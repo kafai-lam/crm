cefe26d594faf67315c9622e446fb30707565b9a
Fai <lamkafai1997@gmail.com>
2025-01-02 16:24:24 -0500

    [FIX] crm_phonecall: fix action button view

--- crm_phonecall/models/res_partner.py
+++ crm_phonecall/models/res_partner.py
@@ -3,6 +3,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
+from odoo.tools.safe_eval import safe_eval
 
 
 class ResPartner(models.Model):
@@ -18,6 +19,24 @@ class ResPartner(models.Model):
     def _compute_phonecall_count(self):
         """Calculate number of phonecalls."""
         for partner in self:
-            partner.phonecall_count = self.env["crm.phonecall"].search_count(
-                [("partner_id", "=", partner.id)]
+            search_domain = (
+                [("partner_id.commercial_partner_id", "=", partner.id)]
+                if partner.is_company
+                else [("partner_id", "=", partner.id)]
             )
+            partner.phonecall_count = self.env["crm.phonecall"].search_count(
+                search_domain
+            )
+
+    def button_open_phonecall(self):
+        self.ensure_one()
+        action = self.sudo().env.ref("crm_phonecall.crm_case_categ_phone_incoming0")
+        action_dict = action.read()[0] if action else {}
+        action_dict["context"] = safe_eval(action_dict.get("context", "{}"))
+        action_dict["context"].update(
+            {
+                "search_default_partner_id": self.id,
+                "default_partner_id": self.id,
+            }
+        )
+        return action_dict
